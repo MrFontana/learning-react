66e970323e14f1276dd26a97305eef3c685ddf38
CCKevinFontana <kfontana@crescendocollective.com>
2023-06-15 08:43:06 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function HelloWorld() {
+	const name = "Kevin";
+	return (
+		<>
+			<div>
+				<h1>This is a hello</h1>
+				Hello <strong>{name}!</strong>
+			</div>
+			<span>Another Element</span>
+		</>
+	);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
