ae8573b7c92612872d7083edf4a639db6c9694bf
CCKevinFontana <kfontana@crescendocollective.com>
2023-06-15 08:55:24 -0700

    showing the js equivalent code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Custom() {
+	return <span>custom</span>;
+}
+
 function HelloWorld() {
 	const name = "Kevin";
+
+	// Javascript equivalent to React
+	// return React.createElement("div", null,
+	// 	React.createElement("h1",
+	// 	null,
+	// 	"this is hello"),
+	// 	"hello ",
+	// 	React.createElement("strong",
+	// 	null,
+	// 	name)
+	// React.createContext(Custom)
+	// );
 	return (
-		<>
-			<div>
-				<h1>This is a hello</h1>
-				Hello <strong>{name}!</strong>
-			</div>
-			<span>Another Element</span>
-		</>
+		<div>
+			<h1>This is a hello</h1>
+			Hello <strong>{name}!</strong>
+			<Custom />
+		</div>
 	);
 }
 
