758899faec91871f9b48156fbf53e9ddcbf6b01f
CCKevinFontana <kfontana@crescendocollective.com>
2023-06-15 10:15:40 -0700

    Created a tweet with multipl components

--- src/index.js
+++ src/index.js
@@ -2,32 +2,48 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function HelloWorld() {
-	const name = "Kevin";
-
-	// Javascript equivalent to React
-	// return React.createElement("div", null,
-	// 	React.createElement("h1",
-	// 	null,
-	// 	"this is hello"),
-	// 	"hello ",
-	// 	React.createElement("strong",
-	// 	null,
-	// 	name)
-	// React.createContext(Custom)
-	// );
+function Tweet() {
 	return (
-		<div className="dark">
-			<h1 onDoubleClick={() => console.log("hello")}>This is a hello</h1>
-			<label htmlFor="email">Emal</label>
-			<input id="email" />
-			{/* this is a comment*/}
-			{
-				// this is also a comment
-			}
-			Hello <strong>{name}!</strong>
+		<div className="tweet">
+			<Avatar />
+			<div className="content">
+				<Author />
+				<Time />
+				<Message />
+				<div className="buttons">
+					<ReplyButton />
+					<RetweetButton />
+					<LikeButton />
+					<MoreOptionsButton />
+				</div>
+			</div>
 		</div>
 	);
 }
 
-ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
+const ReplyButton = () => <i className="fa fa-reply reply-button"></i>;
+const RetweetButton = () => <i className="fa fa-retweet retweet-button"></i>;
+const LikeButton = () => <i className="fa fa-heart like-button"></i>;
+const MoreOptionsButton = () => <i className="fa fa-ellipsis-h more-options-button"></i>;
+
+function Message() {
+	return <div className="message">Something Great.</div>;
+}
+
+function Time() {
+	return <span className="time">3h ago</span>;
+}
+
+function Author() {
+	return (
+		<span className="author">
+			<span className="name">Kevin Fontana</span>
+			<span className="handle">@NXSKevin</span>
+		</span>
+	);
+}
+
+function Avatar() {
+	return <img alt="avatar" className="avatar" src="https://www.gravatar.com/avatar/nothing" />;
+}
+ReactDOM.render(<Tweet />, document.querySelector("#root"));
